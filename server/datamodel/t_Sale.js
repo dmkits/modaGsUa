35b7fa4cc67c5e457c47fdefa9a6c80c9625433f
dmkits <dmk.dp.ua@gmail.com>
2018-11-12 14:21:58 +0200

    cashier sales report. refactor libs and main pages.

---
+++ server/datamodel/t_Sale.js
@@ -0,0 +1,7 @@
+module.exports.id=module.id;
+var modelData = { tableName:"t_Sale", idField:"ChID",
+        fields:["ChID","DocID","DocDate","DocTime","IntDocID","CurrID","KursMC","OurID","StockID","CompID","CRID","OperID","EmpID","DCardID","Notes",
+            "CodeID1","CodeID2","CodeID3","CodeID4","CodeID5","Discount","CreditID","StateCode","DeskCode","Visitors","DocCreateTime",
+            "CashSumCC","ChangeSumCC","TSumCC_nt","TTaxSum","TSumCC_wt","TPurSumCC_nt","TPurTaxSum","TPurSumCC_wt","TRealSum","TLevySum"]
+    };
+module.exports.modelData=modelData;
--- server/modules/reportsCashier.js
+++ server/modules/reportsCashier.js
@@ -2,10 +2,11 @@ var dataModel=require('../datamodel');
 var t_Rem= require(appDataModelPath+"t_Rem");
 var r_Ours= require(appDataModelPath+"r_Ours"), r_Stocks= require(appDataModelPath+"r_Stocks"),
     r_Prods=require(appDataModelPath+"r_Prods"), z_Docs=require(appDataModelPath+"z_Docs"),
+    t_Sale=require(appDataModelPath+"t_Sale"),t_SaleD=require(appDataModelPath+"t_SaleD"),
     queryProdMove=require(appDataModelPath+"queryProdMove");
 
 module.exports.validateModule = function(errs, nextValidateModuleCallback){
-    dataModel.initValidateDataModels([t_Rem,r_Ours,r_Stocks,r_Prods,queryProdMove,z_Docs], errs,
+    dataModel.initValidateDataModels([t_Rem,r_Ours,r_Stocks,r_Prods,t_Sale,t_SaleD,queryProdMove,z_Docs], errs,
         function(){
             nextValidateModuleCallback();
         });
@@ -30,6 +31,43 @@ module.exports.init = function(app){
                 res.send(result);
             });
     });
+    var tProdsSalesTableColumns=[
+        {data: "ChID", name: "ChID", width: 50, type: "text", visible:false, dataSource:"t_SaleD"},
+        {data: "OurID", name: "OurID", width: 50, type: "text", visible:false,
+            dataSource:"t_Sale", sourceField:"DocID", linkCondition:"t_Sale.ChID=t_SaleD.ChID"},
+        {data: "StockID", name: "StockID", width: 50, type: "text", visible:false, dataSource:"t_Sale"},
+        {data: "CRID", name: "CRID", width: 50, type: "text", visible:false, dataSource:"t_Sale"},
+        {data: "CRName", name: "Касса", width: 200, type: "text", visible:true,
+            dataSource:"r_CRs", sourceField:"CRName", linkCondition:"r_CRs.CRID=t_Sale.CRID"},
+        {data: "DocID", name: "Номер чека", width: 70, type: "text", align:"center", visible:true, dataSource:"t_Sale"},
+        {data: "DocDate", name: "Дата чека", width: 55, type: "dateAsText", visible:true, dataSource:"t_Sale"},
+        {data: "DocTime", name: "Дата время чека", width: 55, type: "datetimeAsText", visible:true, dataSource:"t_Sale"},
+        {data: "SrcPosID", name: "Позиция", width: 50, type: "text", align:"right", visible:true, dataSource:"t_SaleD"},
+        {data: "Barcode", name: "Штрихкод", width: 75, type: "text", align:"center", visible:false, dataSource:"t_SaleD"},
+        {data: "ProdID", name: "Код товара", width: 50, type: "text", align:"center", visible:true, dataSource:"t_SaleD"},
+        // {data: "Article1", name: "Артикул1 товара", width: 200, type: "text",
+        //     dataSource:"r_Prods", sourceField:"Article1"},
+        {data: "ProdName", name: "Наименование товара", width: 350, type: "text",
+            dataSource:"r_Prods", sourceField:"ProdName", linkCondition:"r_Prods.ProdID=t_SaleD.ProdID" },
+        {data: "UM", name: "Ед. изм.", width: 55, type: "text", align:"center", dataSource:"t_SaleD" },
+        {data: "Qty", name: "Кол-во", width: 50, type: "numeric",source:"t_SaleD" },
+        {data: "PurPriceCC_wt", name: "Цена без скидки", width: 65, type: "numeric2",source:"t_SaleD" },
+        {data: "RealPrice", name: "Цена", width: 65, type: "numeric2",source:"t_SaleD" },
+        {data: "RealSum", name: "Сумма", width: 75, type: "numeric2",source:"t_SaleD" }
+    ];
+    app.get("/reports/prodsSales/getProductsSales", function(req, res){
+        var conditions={};
+        for(var condItem in req.query) {
+            if(condItem.indexOf("SUM(")<0) conditions["t_Sale."+condItem]=req.query[condItem];
+            else conditions[condItem]=req.query[condItem];
+        }
+        t_SaleD.getDataForTable(req.dbUC,{tableColumns:tProdsSalesTableColumns, identifier:tProdsSalesTableColumns[0].data,
+                conditions:conditions, order:"OurID, StockID, SrcPosID"},
+            function(result){
+                res.send(result);
+            });
+    });
+
     var tProdsRemsTableColumns=[
         {data: "OurID", name: "OurID", width: 50, type: "text", visible:false, dataSource:"t_Rem"},
         {data: "StockID", name: "StockID", width: 50, type: "text", visible:false, dataSource:"t_Rem"},
