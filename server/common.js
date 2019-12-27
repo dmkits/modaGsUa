1030cc284132744119610ab3aad82ff8974748de
admkits <LvbnhbqCBN2017>
2019-12-27 08:41:50 +0200

    Refactor code.

--- server/common.js
+++ server/common.js
@@ -14,20 +14,20 @@ module.exports.getStartupParams= function(){
             var port= arg.replace(sParamName,"");
             if(port>0 && port<65536) startupParams.port= port;
         }else if(arg.charAt(0).toUpperCase()>'A' && arg.charAt(0).toUpperCase()<'Z'){
-            startupParams.mode = arg;
+            startupParams.mode= arg;
         }else if(arg.indexOf(sParamName='-log:')==0){
             var logParam = arg.replace(sParamName,"");
             if(logParam.toLowerCase()=="console") startupParams.logToConsole= true;
             else if(logParam.toLowerCase()=="debug") startupParams.logDebug= true;
         }
     }
-    if(!startupParams.port)startupParams.port= 8080;
-    if(!startupParams.mode)startupParams.mode= 'production';
+    if(!startupParams.port) startupParams.port= 8080;
+    if(!startupParams.mode) startupParams.mode= 'production';
     return startupParams;
 };
 
 module.exports.loadConfig= function(fileName){
-    var stringConfig = fs.readFileSync(sysConfigPath+fileName);
+    var stringConfig= fs.readFileSync(sysConfigPath+fileName);
     return JSON.parse(stringConfig);
 };
 module.exports.saveConfig= function(fileName,dbConfig,callback){
@@ -36,52 +36,46 @@ module.exports.saveConfig= function(fileName,dbConfig,callback){
     })
 };
 
-module.exports.getJSONWithoutComments= function (text){
-    var target = "/*";
-    var pos = 0;
-    while (true) {
-        var foundPos = text.indexOf(target, pos);
-        if (foundPos < 0)break;
-        var comment = text.substring(foundPos, text.indexOf("*/", foundPos)+2);
-        text=text.replace(comment,"");
-        pos = foundPos + 1;
+module.exports.getJSONWithoutComments= function(text){
+    var target= "/*", pos= 0;
+    while(true){
+        var foundPos= text.indexOf(target,pos);
+        if(foundPos<0)break;
+        var comment= text.substring(foundPos,text.indexOf("*/",foundPos)+2);
+        text= text.replace(comment,"");
+        pos= foundPos + 1;
     }
     return text;
 };
 
-module.exports.getUIDNumber=function (){
-    var str= uid.time();
-    var len = str.length;
-    var num = BigNumber(0);
-    for (var i = (len - 1); i >= 0; i--)
+module.exports.getUIDNumber= function(){
+    var str= uid.time(), len= str.length, num= BigNumber(0);
+    for(var i=(len-1); i>=0; i--)
         num.plus(BigNumber(256).pow(i).mult(str.charCodeAt(i)));
     return num.toString();
 };
 
-var getControlBarcodeFigure=function(valueForBarcode){
+var getControlBarcodeFigure= function(valueForBarcode){
     var barcodeStr=valueForBarcode.toString();
     if(barcodeStr.length!=12) return null;
-    var splittedBarcode=barcodeStr.split('');
-    var oddSum=0, evenSum=0;
+    var splittedBarcode=barcodeStr.split(''), oddSum=0, evenSum=0;
     for(var i in splittedBarcode){
         var fig=parseInt(splittedBarcode[i]);
-        if(i%2==0)oddSum = oddSum+fig;
-        else evenSum = evenSum+fig;
+        if(i%2==0) oddSum+= fig; else evenSum+= fig;
     }
     var controlFigure;
-    if(((evenSum*3)+oddSum)%10==0)controlFigure=0;
-    else controlFigure =10-((evenSum*3)+oddSum)%10;
+    if(((evenSum*3)+oddSum)%10==0) controlFigure=0; else controlFigure= 10-((evenSum*3)+oddSum)%10;
     return controlFigure;
 };
-module.exports.getEAN13Barcode=function(code, prefix){
-    var valueForBarcode=Math.pow(10,10)*prefix+code;
-    var barcodeControl=getControlBarcodeFigure(valueForBarcode);
+module.exports.getEAN13Barcode= function(code,prefix){
+    var valueForBarcode= Math.pow(10,10)*prefix+code,
+        barcodeControl= getControlBarcodeFigure(valueForBarcode);
     return valueForBarcode.toString()+barcodeControl.toString();
 };
 
-module.exports.sortArray=function(arr){
-    function compareBychangeDatetime(a, b) {
-        if (new Date(a.changeDatetime) > new Date(b.changeDatetime)) return 1; else return -1;
+module.exports.sortArray= function(arr){
+    function compareBychangeDatetime(a,b){
+        if(new Date(a.changeDatetime) > new Date(b.changeDatetime)) return 1; else return -1;
     }
     arr.sort(compareBychangeDatetime);
     return arr;
