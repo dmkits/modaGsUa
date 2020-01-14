465df823fccc91ede3302b5a97c31c25ae37fc19
admkits <LvbnhbqCBN2017>
2020-01-14 12:20:28 +0200

    Update docs tables columns. Refactor code.

--- server/modules_btk380307/doc_excCashier.js
+++ server/modules_btk380307/doc_excCashier.js
@@ -1,6 +1,6 @@
 var dataModel=require(appDataModelPath), database= require("../databaseMSSQL"), common= require("../common"),
     dateFormat = require('dateformat');
-var t_Exc= require(appDataModelPath+"t_Exc"), t_ExcD= require(appDataModelPath+"t_ExcD");
+var t_Exc= require(appDataModelPath+"t_Exc"), t_ExcD= require(appDataModelPath+"t_ExcD"),
     r_Prods=require(appDataModelPath+"r_Prods");
 var r_DBIs= require(appDataModelPath+"r_DBIs"),
     r_Ours= require(appDataModelPath+"r_Ours"), r_Stocks= require(appDataModelPath+"r_Stocks"),
@@ -10,24 +10,27 @@ var r_DBIs= require(appDataModelPath+"r_DBIs"),
     zf_CanChangeDoc= require(appDataModelPath+"zf_CanChangeDoc");
 
 module.exports.validateModule = function(errs, nextValidateModuleCallback){
-    dataModel.initValidateDataModels([t_Exc,t_ExcD,r_DBIs,r_Ours,r_Stocks,r_Comps,r_Currs,r_States,r_StateDocs,r_StateDocsChange,zf_CanChangeDoc,r_Prods], errs,
-        function(){
-            nextValidateModuleCallback();
-        });
+    dataModel.initValidateDataModels(
+        [t_Exc,t_ExcD,r_DBIs,r_Ours,r_Stocks,r_Comps,r_Currs,r_States,r_StateDocs,r_StateDocsChange,zf_CanChangeDoc,r_Prods], errs,
+        function(){ nextValidateModuleCallback(); }
+    );
 };
 
 module.exports.moduleViewURL = "/docs/excCashier";
 module.exports.moduleViewPath = "docs/doc_excCashier.html";
 module.exports.init = function(app){
     var tExcsListTableColumns=[
-        {data:"ChID", name:"ChID", width:85, type:"text", readOnly:true, visible:false, dataSource:"t_Exc"},
+        {data:"ChID", name:"Код рег.", width:75, type:"text",align:"center", readOnly:true, visible:false, dataSource:"t_Exc"},
         {data:"DocID", name:"Номер", width:85, type:"text", align:"right", dataSource:"t_Exc"},
         {data:"IntDocID", name:"Вн. номер", width:85, type:"text", align:"right", visible:false, dataSource:"t_Exc"},
         {data:"DocDate", name:"Дата", width:60, type:"dateAsText",align:"center", dataSource:"t_Exc"},
+        {data:"OurID", name:"Код фирмы", width:50, type:"text",align:"center", visible:false, dataSource:"t_Exc"},
         {data:"OurName", name:"Фирма", width:150, type:"text", visible:false,
             dataSource:"r_Ours", sourceField:"OurName", linkCondition:"r_Ours.OurID=t_Exc.OurID" },
+        {data:"StockID", name:"Код склада", width:50, type:"text",align:"center", visible:false, dataSource:"t_Exc"},
         {data:"StockName", name:"Склад", width:150, type:"text",
             dataSource:"r_Stocks", sourceField:"StockName", linkCondition:"r_Stocks.StockID=t_Exc.StockID" },
+        {data:"NewStockID", name:"Код нов. склада", width:70, type:"text",align:"center", visible:false, dataSource:"t_Exc"},
         {data:"NewStockName", name:"На склад", width:150, type:"text",
             dataSource:"r_Stocks as st2", sourceField:"StockName", linkCondition:"st2.StockID=t_Exc.NewStockID" },
         {data:"CurrID", name:"Код валюты", width:50, type:"text", align:"center", visible:false, dataSource:"t_Exc", sourceField:"CurrID"},
@@ -41,14 +44,14 @@ module.exports.init = function(app){
         {data:"TSumCC", name:"Сумма", width:85, type:"numeric", dataSource:"t_Exc",sourceField:"TSumCC_wt"},
         {data:"TNewSumCC", name:"Факт. сумма", width:85, type:"numeric",
             dataFunction:{function:"sumIsNull", sourceField:"t_ExcD.NewQty*t_ExcD.PriceCC_wt"} },
-        {data:"StateCode", name:"StateCode", width:50, type:"text", readOnly:true, visible:false, dataSource:"t_Exc"},
+        {data:"StateCode", name:"Код статуса", width:50, type:"text",align:"center", readOnly:true, visible:false, dataSource:"t_Exc"},
         {data:"StateName", name:"Статус", width:250, type:"text",
             dataSource:"r_States", sourceField:"StateName", linkCondition:"r_States.StateCode=t_Exc.StateCode" },
-        {data:"CanChangeDoc", name:"CanChangeDoc", width:50, type:"text", readOnly:true, visible:false,
+        {data:"CanChangeDoc", name:"Изменение разрешено", width:85, type:"text",align:"center", readOnly:true, visible:false,
             dataFunction:"dbo.zf_CanChangeDoc(11021,t_Exc.ChID,t_Exc.StateCode)"},
-        {data:"StateCodeCashierConfirm", name:"StateCodeConfirm", width:50, type:"text", readOnly:true, visible:false,
+        {data:"StateCodeCashierConfirm", name:"StateCodeConfirm", width:0, type:"text", readOnly:true, visible:false,
             dataFunction:"CASE t_Exc.StateCode When 50 Then 51 When 56 Then 57 Else NULL END"},
-        {data:"StateCodeCashierRetrieve", name:"StateCodeRetrieve", width:50, type:"text", readOnly:true, visible:false,
+        {data:"StateCodeCashierRetrieve", name:"StateCodeRetrieve", width:0, type:"text", readOnly:true, visible:false,
             dataFunction:"CASE t_Exc.StateCode When 50 Then 52 When 56 Then 58 Else NULL END"}
     ];
     r_Stocks.getEmpOperStocksList= function(dbUC,empID,calback){
@@ -80,9 +83,8 @@ module.exports.init = function(app){
             if(empOperStocksLists) aconditions["t_Exc.NewStockID in ("+empOperStocksLists+")"]=null; else aconditions["t_Exc.NewStockID is null"]=null;
             t_Exc.getDataForTable(req.dbUC,{tableColumns:tExcsListTableColumns, identifier:tExcsListTableColumns[0].data,
                     conditions:conditions, aconditions:aconditions, order:"DocDate, DocID"},
-                function(result){
-                    res.send(result);
-                });
+                function(result){ res.send(result); }
+            );
         });
     });
     app.get("/docs/excCashier/getExcData", function(req, res){
@@ -102,35 +104,34 @@ module.exports.init = function(app){
         }
         t_Exc.updTableDataItem(req.dbUC,{tableColumns:tExcsListTableColumns, idFieldName:"ChID",
                 updFields:["StateCode"], updTableData:{"ChID":chID,"StateCode":storeData["StateCode"]}},
-            function(result){
-                res.send(result);
-            });
+            function(result){ res.send(result); }
+        );
     });
 
     var tExcDTableColumns=[
-        {data:"ChID", name:"ChID", width:85, type:"text", align:"center", dataSource:"t_ExcD", identifier:true, readOnly:true, visible:false},
+        {data:"ChID", name:"Код рег.", width:75, type:"text", align:"center", dataSource:"t_ExcD", identifier:true, readOnly:true, visible:false},
         {data:"SrcPosID", name:"№ п/п", width:45, type:"numeric", align:"center", dataSource:"t_ExcD", identifier:true, readOnly:true },
-        //{data: "Article1", name:"Артикул1 товара", width:200, readOnly:true,
-        //    dataSource:"r_Prods", sourceField:"Article1", linkCondition:"r_Prods.ProdID=t_ExcD.ProdID"},
+        {data: "Article1", name:"Артикул1 товара", width:200, type:"text",align:"center", readOnly:true, visible:false,
+            dataSource:"r_Prods", sourceField:"Article1", linkCondition:"r_Prods.ProdID=t_ExcD.ProdID"},
         {data:"ProdID", name:"Код товара", width:50, type:"text", align:"center", dataSource:"t_ExcD", visible:true, readOnly:true},
-        {data:"Barcode", name:"Штрихкод", width:75, type:"text", align:"center", dataSource:"t_ExcD", visible:false, readOnly:true},
+        {data:"Barcode", name:"Штрихкод", width:90, type:"text", align:"center", dataSource:"t_ExcD", visible:false, readOnly:true},
         {data:"ProdName", name:"Наименование товара", width:350, type:"text", readOnly:true,
             dataSource:"r_Prods", sourceField:"ProdName", linkCondition:"r_Prods.ProdID=t_ExcD.ProdID" },
-        {data:"UM", name:"Ед. изм.", width:50, type:"text", align:"center", dataSource:"t_ExcD", sourceField:"UM", readOnly:true},
+        {data:"UM", name:"Ед. изм.", width:45, type:"text", align:"center", dataSource:"t_ExcD", sourceField:"UM", readOnly:true},
+        {data:"PPID", name:"Партия", width:50, type:"numeric", align:"center", visible:false, readOnly:true},
         {data:"Qty", name:"Кол-во", width:55, type:"numeric", align:"center", dataSource:"t_ExcD", readOnly:true},
-        {data:"PPID", name:"Партия", width:60, type:"numeric", align:"center", visible:false, readOnly:true},
         {data:"NewQty", name:"Факт кол-во", width:55, type:"numeric", align:"center", dataSource:"t_ExcD", readOnly:false},
         {data:"PriceCC", name:"Цена", width:75, type:"numeric2", readOnly:true, dataSource:"t_ExcD",sourceField:"PriceCC_wt"},
         {data:"ExcChID", name:"ExcChID", width:0, type:"text", visible:false, readOnly:true,
             dataSource:"t_Exc", sourceField:"ChID", linkCondition:"t_Exc.ChID=t_ExcD.ChID" },
         {data:"StockID", name:"StockID", width:0, type:"text", visible:false, readOnly:true,
             dataSource:"r_Stocks", sourceField:"StockID", linkCondition:"r_Stocks.StockID=t_Exc.StockID" },
-        {data:"PLID", name:"PLID", width:50, type:"text", visible:false, readOnly:true,
+        {data:"PLID", name:"Прайс-лист", width:65, type:"text",align:"center", visible:false, readOnly:true,
             dataSource:"r_PLs", sourceField:"PLID", linkCondition:"r_PLs.PLID=r_Stocks.PLID" },
         {data:"PLPriceCC", name:"Цена по прайс-листу", width:75, type:"numeric2", visible:false, readOnly:true,
             dataSource:"r_ProdMP", sourceField:"PriceMC", linkCondition:"r_ProdMP.PLID=r_PLs.PLID and r_ProdMP.ProdID=t_ExcD.ProdID" },
-        {data:"SumCC", name:"Сумма", width:75, type:"numeric2", dataSource:"t_ExcD",sourceField:"SumCC_wt", readOnly:true},
-        {data:"NewSumCC", name:"Факт. сумма", width:75, type:"numeric2", dataFunction:"t_ExcD.NewQty*t_ExcD.PriceCC_wt", readOnly:true}
+        {data:"SumCC", name:"Сумма", width:85, type:"numeric2", dataSource:"t_ExcD",sourceField:"SumCC_wt", readOnly:true},
+        {data:"NewSumCC", name:"Факт. сумма", width:85, type:"numeric2", dataFunction:"t_ExcD.NewQty*t_ExcD.PriceCC_wt", readOnly:true}
     ];
     app.get("/docs/excCashier/getDataForExcDTable", function(req, res){
         var conditions={};
@@ -139,9 +140,8 @@ module.exports.init = function(app){
             else conditions["t_ExcD."+condItem]=req.query[condItem];
         t_ExcD.getDataForDocTable(req.dbUC,{tableColumns:tExcDTableColumns, identifier:tExcDTableColumns[0].data,
                 conditions:conditions, order:"SrcPosID"},
-            function(result){
-                res.send(result);
-            });
+            function(result){ res.send(result); }
+        );
     });
     app.post("/docs/excCashier/storeExcDTableData", function(req,res){
         var storeData=req.body, chID=storeData["ChID"], srcPosID=storeData["SrcPosID"];
@@ -151,8 +151,7 @@ module.exports.init = function(app){
         }
         t_ExcD.updTableDataItem(req.dbUC,{tableColumns:tExcDTableColumns, idFields:["ChID","SrcPosID"],
                 updFields:["NewQty"], updTableData:{"ChID":chID,"SrcPosID":srcPosID,"NewQty":storeData["NewQty"]}},
-            function(result){
-                res.send(result);
-            });
+            function(result){ res.send(result); }
+        );
     });
 };
